2d09b288acca349142a66edb90748b14c10c5088
Delali Kumapley <delalikumapley@Apples-MacBook-Pro-39.local>
2019-08-12 12:03:47 -0400

    Iteration 3 Completed

--- Emojional!/ViewController.swift
+++ Emojional!/ViewController.swift
@@ -12,6 +12,11 @@ class ViewController: UIViewController {
 
     let emojis = ["😔": "Sad face", "😄": "Happy face"]
     
+    let customMessages =
+        ["Sad face": ["I am so sorry to hear that!", "Take a deep breath", "Think of something that makes you feel happy!"],
+         "Happy face": ["Well keep doing what you're doing!", "Find a way to brighten up someone else's day!", "That's great to hear!"]]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -19,11 +24,16 @@ class ViewController: UIViewController {
     @IBAction func showMessage(sender: UIButton) {
         
         if let selectedEmotion = sender.titleLabel?.text {
-            let titleText = "Welcome to my first app!"
+            let titleText = "Welcome to Emojional!"
             let messageText = emojis[selectedEmotion]!
+            let number = Int.random(in : 0 ... 2)
+    
+            let emojiMessage = customMessages[emojis[selectedEmotion]!]?[number]
+          
+        let alertController = UIAlertController(title: "\(titleText)" , message: "\(emojiMessage!)", preferredStyle: UIAlertController.Style.alert)
             
-        let alertController = UIAlertController(title: "\(titleText)" , message: "\(messageText)", preferredStyle: UIAlertController.Style.alert)
-            
+            alertController.addAction(UIAlertAction(title: "Thanks!", style: UIAlertAction.Style.default, handler: nil))
+
             present(alertController, animated: true, completion: nil)
         
         }
